b0125dd8f870cc5c16f65cd364d55549f64701e0
1103409364 <1103409364@qq.com>
2022-04-17 18:28:29 +0800

    chore: 调试代理配置
    Signed-off-by: 1103409364 <1103409364@qq.com>

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,26 @@
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import analyzer from 'rollup-plugin-analyzer'
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import analyzer from "rollup-plugin-analyzer";
 
 export default defineConfig({
   resolve: {
     alias: {
-      src: resolve(__dirname, 'src'),
+      src: resolve(__dirname, "src"),
     },
   },
-  plugins: [
-    vue(),
-    analyzer({ summaryOnly: true }),
-  ],
-})
+  plugins: [vue(), analyzer({ summaryOnly: true })],
+  server: {
+    // host: ENV.VITE_APP_HOST,
+    // port: ENV.VITE_APP_PORT,
+    proxy: {
+      // Record<string, string | ProxyOp 为开发服务器配置代理
+      "/api": {
+        target: "http://localhost:3080",
+        changeOrigin: true,
+        secure: false,
+        // rewrite: (path) => path.replace('/api', ''),
+      },
+    },
+  },
+});
