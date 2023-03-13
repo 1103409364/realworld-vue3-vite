554e48c32d2c670505e10c60a6de8b534cbb475b
wcs <1103409364@qq.com>
2023-03-13 22:48:36 +0800

    config: api 前缀
    Signed-off-by: wcs <1103409364@qq.com>

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,9 +3,7 @@ import FetchRequest from "../utils/request";
 export const limit = 10;
 
 export const request = new FetchRequest({
-  prefix: `${import.meta.env.VITE_APP_API_HOST}:${
-    import.meta.env.VITE_APP_API_PORT
-  }${import.meta.env.VITE_APP_API_PREFIX}`,
+  prefix: `${import.meta.env.VITE_APP_API_PREFIX}`,
   headers: {
     "Content-Type": "application/json",
   },
